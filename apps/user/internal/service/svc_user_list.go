f5c87acc5c98604e1e23f0e2b2832fee9b02cd5f
saeipi <ksert@live.com>
2022-10-05 19:24:52 +0800

    数组对象改用指针

--- apps/user/internal/service/svc_user_list.go
+++ apps/user/internal/service/svc_user_list.go
@@ -18,7 +18,7 @@ func (s *userService) GetUserList(ctx context.Context, req *pb_user.GetUserListR
 	resp = &pb_user.GetUserListResp{List: make([]*pb_user.UserInfo, 0)}
 	var (
 		w    = entity.NewMysqlWhere()
-		list []po.User
+		list []*po.User
 		err  error
 	)
 	w.Query += " AND uid IN(?)"
