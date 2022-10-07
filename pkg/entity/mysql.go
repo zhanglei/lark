86b31ccd769b4977a1ede05118efbd1fd709ba23
saeipi <ksert@live.com>
2022-10-07 23:34:04 +0800

    CreatedTs/UpdatedTs/DeletedTs字段约束

--- pkg/entity/mysql.go
+++ pkg/entity/mysql.go
@@ -3,26 +3,26 @@ package entity
 import "time"
 
 type GormCreatedTs struct {
-	CreatedTs int64 `gorm:"column:created_ts;autoCreateTime:milli" json:"created_ts"`
+	CreatedTs int64 `gorm:"column:created_ts;autoCreateTime:milli;NOT NULL" json:"created_ts"`
 }
 
 type GormUpdatedTs struct {
-	UpdatedTs int64 `gorm:"column:updated_ts;autoUpdateTime:milli" json:"updated_ts"`
+	UpdatedTs int64 `gorm:"column:updated_ts;autoUpdateTime:milli;NOT NULL" json:"updated_ts"`
 }
 
 type GormDeletedTs struct {
-	DeletedTs int64 `gorm:"column:deleted_ts;default:0" json:"deleted_ts"`
+	DeletedTs int64 `gorm:"column:deleted_ts;default:0;NOT NULL" json:"deleted_ts"`
 }
 
 type GormEntityTs struct {
-	CreatedTs int64 `gorm:"column:created_ts;autoCreateTime:milli" json:"created_ts"`
-	UpdatedTs int64 `gorm:"column:updated_ts;autoUpdateTime:milli" json:"updated_ts"`
-	DeletedTs int64 `gorm:"column:deleted_ts;default:0" json:"deleted_ts"`
+	CreatedTs int64 `gorm:"column:created_ts;autoCreateTime:milli;NOT NULL" json:"created_ts"`
+	UpdatedTs int64 `gorm:"column:updated_ts;autoUpdateTime:milli;NOT NULL" json:"updated_ts"`
+	DeletedTs int64 `gorm:"column:deleted_ts;default:0;NOT NULL" json:"deleted_ts"`
 }
 
 type GormTs struct {
-	CreatedTs int64 `gorm:"column:created_ts;autoCreateTime:milli" json:"created_ts"`
-	UpdatedTs int64 `gorm:"column:updated_ts;autoUpdateTime:milli" json:"updated_ts"`
+	CreatedTs int64 `gorm:"column:created_ts;autoCreateTime:milli;NOT NULL" json:"created_ts"`
+	UpdatedTs int64 `gorm:"column:updated_ts;autoUpdateTime:milli;NOT NULL" json:"updated_ts"`
 }
 
 func Deleted() (column string, value interface{}) {
