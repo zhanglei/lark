3bef4d34b0540e89f98ef3ea88747c6775f72d5a
saeipi <ksert@live.com>
2022-10-07 23:18:18 +0800

    chats表字段约束

--- scripts/mysql/lark_user/sqls/20220701123003_create_table_chats.sql
+++ scripts/mysql/lark_user/sqls/20220701123003_create_table_chats.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS `chats`;
 CREATE TABLE `chats` (
-  `chat_id` bigint NOT NULL COMMENT 'chat ID',
-  `creator_uid` bigint NOT NULL COMMENT '创建者 uid',
+  `chat_id` bigint unsigned NOT NULL COMMENT 'chat ID',
+  `creator_uid` bigint unsigned NOT NULL COMMENT '创建者 uid',
   `chat_hash` CHAR(32) NOT NULL COMMENT 'chat hash值',
-  `chat_type` tinyint(1) NOT NULL  COMMENT 'chat type 1:私聊/2:群聊',
+  `chat_type` tinyint(1) unsigned NOT NULL  COMMENT 'chat type 1:私聊/2:群聊',
   `avatar_key` varchar(64) NOT NULL COMMENT '小图 72*62',
   `title` varchar(128) DEFAULT '' COMMENT 'chat标题',
   `about` varchar(255) DEFAULT '' COMMENT '关于',
@@ -13,6 +13,7 @@ CREATE TABLE `chats` (
   PRIMARY KEY (`chat_id`),
   UNIQUE KEY `chat_id` (`chat_id`),
   KEY `idx_deletedTs` (`deleted_ts`),
+  KEY `idx_creatorUid` (`creator_uid`),
   KEY `idx_chatHash` (`chat_hash`),
   KEY `idx_chatType` (`chat_type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
