881ac39c09c1226cb83f1f40d5917949bdcb3f3e
saeipi <ksert@live.com>
2022-10-07 23:28:50 +0800

    messages表字段约束

--- scripts/mysql/lark_chat_msg/sqls/20220701123001_create_table_messages.sql
+++ scripts/mysql/lark_chat_msg/sqls/20220701123001_create_table_messages.sql
@@ -1,42 +1,20 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : lark_msg
- Source Server Type    : MySQL
- Source Server Version : 80029
- Source Host           : localhost:13307
- Source Schema         : lark_msg
-
- Target Server Type    : MySQL
- Target Server Version : 80029
- File Encoding         : 65001
-
- Date: 09/08/2022 18:24:03
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for messages
--- ----------------------------
 DROP TABLE IF EXISTS `messages`;
 CREATE TABLE `messages` (
-  `srv_msg_id` bigint NOT NULL COMMENT '服务端消息号',
-  `cli_msg_id` bigint NOT NULL DEFAULT '0' COMMENT '客户端消息号',
-  `root_id` bigint NOT NULL DEFAULT '0' COMMENT '根消息id',
-  `parent_id` bigint NOT NULL DEFAULT '0' COMMENT '父消息的id',
-  `upper_message_id` bigint NOT NULL DEFAULT '0' COMMENT '合并转发消息中，上一层级的消息id srv_msg_id',
-  `sender_id` bigint NOT NULL DEFAULT '0' COMMENT '发送者uid',
-  `receiver_id` bigint NOT NULL DEFAULT '0' COMMENT '接收者uid',
-  `sender_platform` tinyint(1) NOT NULL DEFAULT '0' COMMENT '发送者平台',
-  `chat_id` bigint NOT NULL DEFAULT '0' COMMENT '会话ID',
-  `chat_type` tinyint(1) NOT NULL DEFAULT '0' COMMENT '会话类型',
-  `seq_id` int NOT NULL DEFAULT '0' COMMENT '消息唯一ID',
-  `msg_from` int NOT NULL DEFAULT '0' COMMENT '消息来源',
-  `msg_type` int NOT NULL DEFAULT '0' COMMENT '消息类型',
+  `srv_msg_id` bigint unsigned NOT NULL COMMENT '服务端消息号',
+  `cli_msg_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '客户端消息号',
+  `root_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '根消息id',
+  `parent_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '父消息的id',
+  `upper_message_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '合并转发消息中，上一层级的消息id srv_msg_id',
+  `sender_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '发送者uid',
+  `receiver_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '接收者uid',
+  `sender_platform` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '发送者平台',
+  `chat_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '会话ID',
+  `chat_type` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '会话类型',
+  `seq_id` int unsigned NOT NULL DEFAULT '0' COMMENT '消息唯一ID',
+  `msg_from` int unsigned NOT NULL DEFAULT '0' COMMENT '消息来源',
+  `msg_type` int unsigned NOT NULL DEFAULT '0' COMMENT '消息类型',
   `body` text CHARACTER SET utf8mb4 NOT NULL COMMENT '消息本体',
-  `status` tinyint NOT NULL DEFAULT '0' COMMENT '消息状态',
+  `status` tinyint unsigned NOT NULL DEFAULT '0' COMMENT '消息状态',
   `sent_ts` bigint NOT NULL DEFAULT '0' COMMENT '客户端本地发送时间',
   `srv_ts` bigint NOT NULL DEFAULT '0' COMMENT '服务端接收消息的时间',
   `updated_ts` bigint NOT NULL DEFAULT '0' COMMENT '更新时间',
@@ -45,7 +23,7 @@ CREATE TABLE `messages` (
   UNIQUE KEY `srv_msg_id` (`srv_msg_id`),
   UNIQUE KEY `chatId_seqId` (`chat_id`,`seq_id`),
   KEY `idx_deletedTs` (`deleted_ts`),
-  KEY `idx_chatId_seqId` (`chat_id`,`seq_id`)
+  KEY `idx_chatId` (`chat_id`),
+  KEY `idx_chatType` (`chat_type`),
+  KEY `idx_seqId` (`seq_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-SET FOREIGN_KEY_CHECKS = 1;
