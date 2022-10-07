f3493f5084b7d26567696c7be153c8993ebfd0ed
saeipi <ksert@live.com>
2022-10-07 23:16:11 +0800

    users表字段约束

--- scripts/mysql/lark_user/sqls/20220701123001_create_table_users.sql
+++ scripts/mysql/lark_user/sqls/20220701123001_create_table_users.sql
@@ -1,19 +1,19 @@
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `uid` bigint NOT NULL COMMENT '用户ID 系统生成',
+  `uid` bigint unsigned NOT NULL COMMENT '用户ID 系统生成',
   `lark_id` varchar(32) NOT NULL DEFAULT '' COMMENT '账户ID 用户设置',
   `password` varchar(32) NOT NULL DEFAULT '' COMMENT '密码',
   `udid` varchar(40) NOT NULL DEFAULT '' COMMENT '注册设备唯一标识',
-  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '用户状态',
+  `status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '用户状态',
   `nickname` varchar(64) NOT NULL DEFAULT '' COMMENT '昵称',
   `firstname` varchar(32) NOT NULL DEFAULT '' COMMENT 'firstname',
   `lastname` varchar(32) NOT NULL DEFAULT '' COMMENT 'lastname',
-  `gender` tinyint(1) NOT NULL DEFAULT '0' COMMENT '性别',
+  `gender` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '性别',
   `birth_ts` bigint NOT NULL DEFAULT '0' COMMENT '生日',
   `email` varchar(64) NOT NULL DEFAULT '' COMMENT 'Email',
   `mobile` varchar(32) NOT NULL DEFAULT '' COMMENT '手机号',
-  `reg_platform` tinyint(1) NOT NULL DEFAULT '0' COMMENT '注册平台',
-  `platform` tinyint(1) NOT NULL DEFAULT '0' COMMENT '最后登录的平台',
+  `reg_platform` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '注册平台',
+  `platform` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '最后登录的平台',
   `server_id` int NOT NULL DEFAULT '0' COMMENT '分配的ws服务器',
   `city_id` int NOT NULL DEFAULT '0' COMMENT '城市ID',
   `avatar_key` varchar(64) DEFAULT '' COMMENT '小图 72*62',
@@ -29,7 +29,6 @@ CREATE TABLE `users` (
   KEY `idx_mobile` (`mobile`),
   KEY `idx_platform` (`platform`),
   KEY `idx_gender` (`gender`),
-  KEY `id_gender_cityId` (`gender`,`city_id`),
-  KEY `id_larkId_password` (`lark_id`,`password`),
-  KEY `id_mobile_password` (`mobile`,`password`)
+  KEY `idx_cityId` (`city_id`),
+  KEY `idx_password` (`password`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
